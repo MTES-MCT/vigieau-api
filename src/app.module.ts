08febd483f8b482b51f8a74b90a3306eb8b11674
Vincent Lainé <vincent.laine.utc@gmail.com>
2024-03-23 19:24:12 +0100

    Secu + connexion BDD 🔒

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,76 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { LoggerModule } from './logger/logger.module';
 import { ConfigModule } from '@nestjs/config';
+import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerInterceptor } from './core/interceptor/logger.interceptor';
+import { DataSource } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    LoggerModule
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        url: `postgres://${process.env.DATABASE_USER}:${
+          process.env.DATABASE_PASSWORD
+        }@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${
+          process.env.DATABASE_NAME
+        }?${process.env.DATABASE_SSL_CERT ? 'sslmode=require' : ''}`,
+        entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+        logging: ['test', 'dev', 'review', 'local'].includes(
+          process.env.NODE_ENV,
+        )
+          ? ['error', 'schema']
+          : false,
+        migrations: [`${__dirname}/migrations/**/*{.ts,.js}`],
+        cli: {
+          migrationsDir: 'src/migrations',
+        },
+        synchronize: false,
+        maxQueryExecutionTime: 1000,
+        ssl: process.env.NODE_ENV !== 'local',
+        extra:
+          process.env.NODE_ENV !== 'local'
+            ? {
+              ssl: {
+                rejectUnauthorized: false,
+              },
+            }
+            : {},
+      }),
+      dataSourceFactory: async (options) => {
+        const dataSource = await new DataSource(options).initialize();
+        await dataSource.synchronize();
+        await dataSource.runMigrations();
+        return dataSource;
+      },
+    }),
+    // Rate limit, 300 requêtes maximum toutes les 15min par IP
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60 * 15,
+        limit: 300,
+      },
+    ]),
+    LoggerModule,
+    SubscriptionsModule
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggerInterceptor,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
---
+++ src/core/interceptor/logger.interceptor.ts
@@ -0,0 +1,37 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { VigieauLogger } from '../../logger/vigieau.logger';
+const crypto = require('crypto');
+
+@Injectable()
+export class LoggerInterceptor implements NestInterceptor {
+  private readonly _logger = new VigieauLogger('LoggerInterceptor');
+
+  /**
+   * Mise en forme des Logs de l'application
+   * @param context
+   * @param next
+   */
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const id = crypto.randomBytes(8).toString('hex');
+    const req = context.getArgByIndex(0);
+    const body = { ...req.body };
+    this._logger.log(
+      `REQUEST - ${id} - ${req.ip} - ${req.session?.user?.email} - ${
+        req.method
+      } - ${req.originalUrl} - ${!body ? '' : JSON.stringify(body)}`,
+    );
+
+    return next.handle().pipe(
+      tap(() => {
+        this._logger.log(`RESPONSE - ${id}`);
+      }),
+    );
+  }
+}
